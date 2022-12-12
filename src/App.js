bc059e85f60a1019ebd33b0de5a8a1629eab0453
MeowSoul <agent_hammer@mail.ru>
2022-12-12 22:51:16 +0500

    New version

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./css/app.css"
+import img from "./img/photomode_11122022_113758.png"
+function App() {
+  return (
+   <div className="items">
+     <div className="item">
+       <img src={img} alt=""/>
+     </div>
+   </div>
+  );
+}
+
+export default App;
