301fc2a50da332836c77d9a05057f786c04848a7
MeowSoul <agent_hammer@mail.ru>
2022-12-17 23:29:03 +0500

    Использую созданный компонент Card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./css/app.css"
 import img from "./img/photomode_11122022_113758.png"
+import Card from "./componets/card";
+
 function App() {
   return (
    <div className="items">
-     <div className="item">
-       <img src={img} alt=""/>
-     </div>
+       <Card/>
+       <Card/>
+       <Card/>
+       <Card/>
    </div>
   );
 }
