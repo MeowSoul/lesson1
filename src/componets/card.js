b143b82ce68878c77fabe8d3ebc57d2379266d5b
MeowSoul <agent_hammer@mail.ru>
2022-12-17 23:29:59 +0500

    Создан компонент Card

---
+++ src/componets/card.js
@@ -0,0 +1,11 @@
+import img from "../img/photomode_11122022_113758.png";
+
+export default function Card()
+{
+    return (
+        <div className="item">
+            <img src={img} alt=""/>
+        </div>
+    );
+}
+
